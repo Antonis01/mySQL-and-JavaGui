7e158154dc9cb9c62d8bad93f3b692b9b23a77f9
Antonis <antonisanastasiou2012@gmail.com>
2023-01-10 18:03:46 +0200

    fixed stop_update trigger.

--- triggers.sql
+++ triggers.sql
@@ -86,25 +86,25 @@ DELIMITER ;
 DROP TRIGGER IF EXISTS stop_update;
 DELIMITER $
 
-CREATE TRIGGER stop_update AFTER INSERT ON trip
+CREATE TRIGGER stop_update BEFORE UPDATE ON trip
 FOR EACH ROW
 BEGIN
 	
-	#IF (SELECT COUNT(*) FROM reservation WHERE res_tr_id = NEW.tr_id) > 0 THEN
+	IF (SELECT COUNT(*) FROM reservation WHERE res_tr_id = NEW.tr_id) > 0 THEN
 		
-		IF NEW.tr_departure <> tr_departure THEN
+		IF NEW.tr_departure <> old.tr_departure THEN
                 	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You cannot update the date of departure';
         	END IF; 
 
-        	IF NEW.tr_return <> tr_return THEN
+        	IF NEW.tr_return <> old.tr_return THEN
                 	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You cannot update the date of return';
         	END IF; 
 
-        	IF NEW.tr_cost <> tr_cost THEN
+        	IF NEW.tr_cost <> old.tr_cost THEN
                 	SIGNAL SQLSTATE '45000' SET MESSAGE_TEXT = 'You cannot update the cost of the trip';
         	END IF; 
 	
-	#END IF;
+	END IF;
 END $
 
 DELIMITER ;
@@ -114,7 +114,7 @@ DELIMITER ;
 DROP TRIGGER IF EXISTS prevent_salary_lowering;
 DELIMITER $ 
 
-CREATE TRIGGER prevent_salary_lowering
+CREATE TRIGGER prevent_salary_lowering 
  BEFORE UPDATE ON worker
  FOR EACH ROW
  BEGIN
